29e85eeb6fcba00b842daacb1f5313ce276cbaa9
pesrisaw <86398888+TroJanBoi@users.noreply.github.com>
2024-09-17 17:22:30 +0700

    clear

--- wwwroot/js/sidebar.js
+++
@@ -1,6 +0,0 @@
-let sidebar = document.querySelector(".sidebar");
-let sidebarBtn = document.querySelector(".bx-chevrons-right");
-//console.log(sidebarBtn);
-sidebarBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-});
