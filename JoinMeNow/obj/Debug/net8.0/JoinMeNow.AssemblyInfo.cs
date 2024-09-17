7e45984b6eb1d541e9a688b361ec661a28d2116d
pesrisaw <86398888+TroJanBoi@users.noreply.github.com>
2024-09-17 21:54:17 +0700

    update

--- JoinMeNow/obj/Debug/net8.0/JoinMeNow.AssemblyInfo.cs
+++ JoinMeNow/obj/Debug/net8.0/JoinMeNow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JoinMeNow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23075098e485f3487449a5eb35b3a3a22d3101ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c0631df26704263de0f81ee1e08cfdde25669e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("JoinMeNow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JoinMeNow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
