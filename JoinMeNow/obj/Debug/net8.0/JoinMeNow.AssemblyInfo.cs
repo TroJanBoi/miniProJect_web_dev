4b3ca755245655525b4b58e1a7b252fac421b8b4
Narongrit159 <narongrit133@gmail.com>
2024-09-23 09:51:37 +0700

    update

--- JoinMeNow/obj/Debug/net8.0/JoinMeNow.AssemblyInfo.cs
+++ JoinMeNow/obj/Debug/net8.0/JoinMeNow.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JoinMeNow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b9f93901040e71750f923e5784a844581d65b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("JoinMeNow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JoinMeNow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
